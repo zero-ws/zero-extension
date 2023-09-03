79d9bb1b673c1e09165c55de27ad2ff037b08175
silentbalanceyh <silentbalanceyh@126.com>
2023-09-04 04:34:07 +0800

    POM Upgrade

--- zero-fm/src/main/modulat/io/vertx/mod/fm/cv/em/EmDebt.java
+++ zero-fm/src/main/modulat/io/vertx/mod/fm/cv/em/EmDebt.java
@@ -4,7 +4,10 @@ package io.vertx.mod.fm.cv.em;
  * @author lang : 2023-09-01
  */
 public class EmDebt {
-    /* 对应新的单据类型 */
+    /*
+     * 对应新的单据类型
+     * Debt 单据的类型，可以表示应收单，也可以表示退款单
+     * */
     public enum Type {
         DEBT,       // 应收单
         REFUND,     // 退款单
