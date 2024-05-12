b06207257b8758c09b8c3eeb043abf8ae40329f2
silentbalanceyh <silentbalanceyh@126.com>
2024-05-12 09:20:10 +0800

    MDEntity / MDConnect

--- Zero.Extension.Runtime.Ambient.DB/src/test/java/io/zerows/core/web/model/uca/boot/EquipAtTestCase.java
+++ Zero.Extension.Runtime.Ambient.DB/src/test/java/io/zerows/core/web/model/uca/boot/EquipAtTestCase.java
@@ -1,18 +1,18 @@
 package io.zerows.core.web.model.uca.boot;
 
-import io.zerows.core.feature.web.mbse.atom.io.MDConfiguration;
-import io.zerows.core.feature.web.mbse.uca.normalize.EngageAt;
+import io.zerows.core.web.model.atom.io.MDConfiguration;
+import io.zerows.core.web.model.uca.normalize.EquipAt;
 import org.junit.Test;
 
 /**
  * @author lang : 2024-05-08
  */
-public class EngageAtTestCase {
+public class EquipAtTestCase {
 
     @Test
     public void testExtensionAt() {
         final MDConfiguration configuration = new MDConfiguration("zero-extension-runtime-ambient");
-        final EngageAt component = EngageAt.of(configuration.id());
+        final EquipAt component = EquipAt.of(configuration.id());
         component.initialize(configuration);
 
         System.out.println(configuration.id());
